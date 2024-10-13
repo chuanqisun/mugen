d0211ca63b4e04dcc592a29e5aed75791f4e23d9
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2024-10-13 13:58:44 -0700

    make sure we don't double start sessions

--- src/lib/web-speech/speech-to-text.ts
+++ src/lib/web-speech/speech-to-text.ts
@@ -8,6 +8,9 @@ export interface SimpleRecognitionEvent {
 
 export const $recognition = new Subject<SimpleRecognitionEvent>();
 const recognition = new webkitSpeechRecognition();
+
+// Prevent starting multiple sessions
+let isStarted = false;
 recognition.interimResults = true;
 
 export const recognizer = {
@@ -17,6 +20,8 @@ export const recognizer = {
 };
 
 function initSession() {
+  isStarted = true;
+
   recognition.continuous = true;
   recognition.onstart = () => console.log("[recognition] session stated");
   recognition.onresult = (e) => {
@@ -31,6 +36,7 @@ function initSession() {
     console.error(`[recognition] sliently omit error`, e);
   };
   recognition.onend = () => {
+    isStarted = false;
     recognition.stop();
     console.log("[recognition] session ended");
     if (recognition.continuous) initSession();
@@ -38,6 +44,7 @@ function initSession() {
 }
 
 function start() {
+  if (isStarted) return;
   initSession();
   recognition.start();
 }
