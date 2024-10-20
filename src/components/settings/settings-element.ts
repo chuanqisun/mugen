f9c260ecc53220af2198fb968afe65808b2bc58c
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2024-10-20 13:44:45 -0700

    refactor

---
+++ src/components/settings/settings-element.ts
@@ -0,0 +1,31 @@
+import { html, render } from "lit";
+import { fromEvent, map, tap } from "rxjs";
+import { toTargetValueString } from "../../lib/event";
+import { $apiKey, setApiKey } from "../chat-provider/auth";
+
+export class SettingsElement extends HTMLElement {
+  connectedCallback() {
+    render(
+      html`
+        <div class="rows">
+          <label>API Key</label>
+          <input name="api-key" type="password" />
+          <form method="dialog">
+            <button>OK</button>
+          </form>
+        </div>
+      `,
+      this
+    );
+
+    // static elements
+    const apiKeyInput = this.querySelector(`[name="api-key"]`) as HTMLInputElement;
+    // initialize api key input
+    apiKeyInput.value = $apiKey.value;
+    fromEvent<KeyboardEvent>(apiKeyInput, "input").pipe(map(toTargetValueString), tap(setApiKey)).subscribe();
+  }
+}
+
+export function defineSettingsElement() {
+  customElements.define("settings-element", SettingsElement);
+}
--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,25 @@ import { fromEvent, switchMap, tap } from "rxjs";
 import { defineChatInputElement } from "./components/chat-input/chat-input-element";
 import { CodeEditorElement, defineCodeEditorElement } from "./components/code-editor/code-editor-element";
 import { readFile } from "./components/file-system/file-system";
-import { definePopoverElement } from "./components/popover-element/popover-element";
+import { definePopoverElement } from "./components/popover/popover-element";
+import { defineSettingsElement } from "./components/settings/settings-element";
 import { defineTaskElement } from "./components/thread/task-element";
 import { defineThreadElement } from "./components/thread/thread-element";
-import { useMenu } from "./components/use-menu";
 
 defineChatInputElement();
-defineThreadElement();
-defineTaskElement();
 defineCodeEditorElement();
 definePopoverElement();
-
-useMenu();
+defineSettingsElement();
+defineTaskElement();
+defineThreadElement();
 
 fromEvent<CustomEvent>(window, "open-file")
   .pipe(
-    switchMap(async (event) => {
-      const sourceCode = await readFile(event.detail);
-      return sourceCode;
-    }),
-    tap((sourceCode) => {
-      document.querySelector<CodeEditorElement>("code-editor-element")!.value = sourceCode;
-    })
+    switchMap(async (event) => readFile(event.detail)),
+    tap((sourceCode) => (document.querySelector<CodeEditorElement>("code-editor-element")!.value = sourceCode))
   )
+  .subscribe();
+
+fromEvent<MouseEvent>(document.querySelector("#open-settings")!, "click")
+  .pipe(tap(() => document.querySelector<HTMLDialogElement>("#settings-dialog")!.showModal()))
   .subscribe();
