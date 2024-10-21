3272f809a32923c76d06a54d9c628f95cdccec4d
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2024-10-20 20:36:31 -0700

    prep for multi-plex streaming

--- src/components/interpreter/run.ts
+++ src/components/interpreter/run.ts
@@ -1,4 +1,6 @@
-import { EMPTY, endWith, filter, from, map, Observable, Subject, switchMap, tap, withLatestFrom } from "rxjs";
+import { Parser } from "htmlparser2";
+import { ChatCompletionChunk } from "openai/resources/index.mjs";
+import { filter, from, map, Observable, Subject, switchMap, tap, withLatestFrom } from "rxjs";
 import { system, user } from "../../lib/message";
 import { $promptSubmissions } from "../chat-input/submission";
 import { $openai } from "../chat-provider/openai";
@@ -39,24 +41,63 @@ your reponse here...
       }))
   ),
   switchMap(({ stream, submission }) =>
-    from(stream).pipe(
-      map((chunk) => chunk.choices[0].delta.content),
-      filter(Boolean),
-      tap((delta) => $rawPartialResponses.next({ runId: submission.id, delta })),
-      endWith("\0")
-    )
-  ),
-  filter(Boolean),
-  tap((textChunk) => {
-    console.log(textChunk);
-  })
+    parseHtmlStream(submission.id, stream, {
+      onRaw: (raw) => $rawPartialResponses.next({ runId: submission.id, delta: raw }),
+    })
+  )
 );
+
+export interface ParseHtmlStreamOptions {
+  onRaw?: (raw: string) => void;
+}
 
 export interface PartialResponse {
   runId: number;
   delta?: string;
   objectPath?: string;
 }
-export function getPartialResponses(runId: number, rawStream: AsyncIterable<string>): Observable<PartialResponse> {
-  return EMPTY;
+
+export function parseHtmlStream(runId: number, rawStream: AsyncIterable<ChatCompletionChunk>, options?: ParseHtmlStreamOptions): Observable<PartialResponse> {
+  return new Observable<PartialResponse>((subscriber) => {
+    let currentObjectPath = "raw.txt";
+    const parser = new Parser({
+      onopentag(name, attributes) {
+        if (name === "response-file") {
+          currentObjectPath = attributes.path ?? "raw.txt";
+        } else {
+          const attributesString = Object.entries(attributes)
+            .map(([key, value]) => `${key}="${value}"`)
+            .join(" ");
+          subscriber.next({ runId, objectPath: currentObjectPath, delta: `<${name}${attributesString.length ? ` ${attributesString}` : ""}}>` });
+        }
+      },
+      ontext(text) {
+        subscriber.next({ runId, objectPath: currentObjectPath, delta: text });
+      },
+      onclosetag(name) {
+        if (name === "response-file") {
+          currentObjectPath = "raw.txt";
+        } else {
+          subscriber.next({ runId, objectPath: currentObjectPath, delta: `</${name}>` });
+        }
+      },
+    });
+
+    const sub = from(rawStream)
+      .pipe(
+        map((chunk) => chunk.choices[0].delta.content),
+        filter(Boolean),
+        tap({
+          next: (chunk) => {
+            parser.write(chunk), options?.onRaw?.(chunk);
+          },
+          finalize: () => parser.end(),
+        })
+      )
+      .subscribe();
+
+    return () => {
+      sub.unsubscribe();
+    };
+  });
 }
