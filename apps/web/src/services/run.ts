e0fb60f32095f6bb460ea4a57cad00781d293b2b
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2024-11-02 16:10:19 -0700

    artifact poc

--- apps/web/src/services/run.ts
+++ apps/web/src/services/run.ts
@@ -1,3 +1,4 @@
+import { Parser } from "htmlparser2";
 import { $chat } from "./chat";
 import { $thread, appendAssistantMessage, createAssistantMessage } from "./thread";
 
@@ -5,11 +6,19 @@ export async function run(id: number) {
   const currentItems = $thread.value.items;
   const runItems = currentItems.slice(0, currentItems.findIndex((item) => item.id === id) + 1);
 
+  const parser = new Parser({});
+
   const stream = $chat.value.messages
     .stream({
       temperature: 0,
       max_tokens: 4_000,
       model: "claude-3-haiku-20240307",
+      system: `Respond in plaintext. If user asks for standalone code or document, wrap them with embedded <standalone-artifact> tags.
+Requirements:
+- Each <standalone-artifact> must have path with extension, e.g. <standalone-artifact path="[filename].[extension]">...</standalone-artifact>.
+- Suppported file extensions: txt, md, html, css, js, ts, jsx, tsx, json, sh
+- Do NOT explain the artifact
+      `.trim(),
       messages: runItems.map((item) => ({
         role: item.role,
         content: item.content,
