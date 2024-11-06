d39684312611dd4caec08ba1f65c5824881fe973
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2024-11-05 21:07:16 -0800

    handle events

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,8 +1,61 @@
-
-/** Element queries */
+import { filter, fromEvent, map, merge, share, Subject, tap } from "rxjs";
+import { defineSettingsElement } from "./elements/settings-element";
+import { parseKeyboardShortcut, preventDefault, stopPropagation, toTargetValueString } from "./utils/event";
+import { $ } from "./utils/query";
+import { parseCommand } from "./utils/string";
 
 /** Define custom elements */
+defineSettingsElement();
 
-/** Pure stream processing */
+/** Element queries */
+const promptInputElement = $<HTMLInputElement>("#prompt-input")!;
+const settingsDialogElement = $<HTMLDialogElement>("#settings-dialog")!;
 
-/** Render to DOM */
+/** Declare subjects */
+const $programmaticCommandSubmission = new Subject<string>();
+
+/** Stream processing */
+const $keyboardShortcut = fromEvent<KeyboardEvent>(document, "keydown").pipe(map(parseKeyboardShortcut), filter(Boolean));
+
+const $textInputSubmission = fromEvent<KeyboardEvent>(promptInputElement, "keydown").pipe(
+  filter((event) => event.key === "Enter"),
+  tap(preventDefault),
+  tap(stopPropagation),
+  map(toTargetValueString),
+  map((value) => value.trim()),
+  filter(Boolean),
+  tap(() => (promptInputElement.value = "")),
+  share()
+);
+
+const $textInputCommandSubmission = $textInputSubmission.pipe(filter((content) => content.startsWith("/")));
+
+const $parsedCommand = merge($textInputCommandSubmission, $programmaticCommandSubmission).pipe(map(parseCommand));
+
+const $chatSubmission = $textInputSubmission.pipe(filter((content) => !content.startsWith("/")));
+
+const $handleCommand = $parsedCommand.pipe(
+  tap((parsed) => {
+    console.log(parsed);
+    switch (parsed.command) {
+      case "login":
+        return settingsDialogElement.showModal();
+      case "focusPrompt":
+        return promptInputElement.focus();
+    }
+  })
+);
+
+const $handleKeyboardShortcut = $keyboardShortcut.pipe(
+  tap((parsed) => {
+    switch (parsed.combo) {
+      case "Ctrl+KeyK":
+        parsed.event.preventDefault();
+        return $programmaticCommandSubmission.next("/focusPrompt");
+    }
+  })
+);
+
+/** I/O effects */
+$handleCommand.subscribe();
+$handleKeyboardShortcut.subscribe();
