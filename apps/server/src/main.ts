622474bfbd37b5b7216646525e15f0721e76cf1a
Chauqni Sun <chuanqi.sun.dev@gmail.com>
2024-12-13 17:39:56 -0800

    poc node spawn sim

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,21 +1,57 @@
-import { initTRPC } from "@trpc/server";
-import { createHTTPServer } from "@trpc/server/adapters/standalone";
-import * as z from "zod";
+import cors from "@fastify/cors";
+import { exec, spawn } from "child_process";
+import Fastify from "fastify";
+import { FastifySSEPlugin } from "fastify-sse-v2";
 
-export type AppRouter = typeof appRouter;
+async function main() {
+  const fastify = Fastify();
+  fastify.register(cors);
+  fastify.register(FastifySSEPlugin);
 
-const t = initTRPC.create();
-const appRouter = t.router({
-  run: t.procedure.input(z.string()).query(async (opts) => {
-    const cmd = opts.input;
-    console.log("run command", cmd);
-  }),
-});
+  fastify.post("/spawn", {}, (request, reply) => {
+    const command = (request.body as any).command;
+    const [programName, ...args] = command.split(" ");
+    console.log({ programName, args });
 
-const server = createHTTPServer({
-  router: appRouter,
-});
+    const proc = spawn(programName, args);
+    proc.stdout.on("data", (data) => {
+      const textData = data.toString();
+      reply.sse({ data: textData });
+    });
+    proc.stderr.on("data", (data) => console.error(data.toString()));
+    proc.on("error", (err) => console.error(err));
+    proc.on("close", (_code) => {
+      reply.sse({ event: "close" });
+    });
+  });
 
-server.listen(3000);
+  fastify.post("/exec", {}, (request, reply) => {
+    const command = (request.body as any).command;
 
-console.log("mugen server running on http://localhost:3000");
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`error: ${error.message}`);
+        reply.sse({ event: "close" });
+        return;
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      reply.sse({ data: stdout });
+      reply.sse({ event: "close" });
+    });
+  });
+
+  try {
+    await fastify.listen({ port: 3000 });
+
+    console.log("mugen server running on http://localhost:3000");
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+main();
--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,1 +1,41 @@
-console.log("hello from main");
+import "./style.css";
+
+import { filter, fromEvent, tap } from "rxjs";
+import { $ } from "./lib/dom";
+import { handleSSEResponse } from "./lib/sse";
+
+const backendHost = "http://localhost:3000";
+const stdout = $<HTMLElement>("#stdout")!;
+const input = $("textarea")!;
+
+// enter to submit
+const submitCommand$ = fromEvent(input, "keydown").pipe(
+  filter((e) => (e as KeyboardEvent).key === "Enter"),
+  tap((e) => e.preventDefault()),
+  tap(() => {
+    const command = input.value;
+
+    stdout.innerText += `$ ${command}\n`;
+
+    input.value = "";
+
+    const sse = fetch(`${backendHost}/exec`, {
+      method: "POST",
+      body: JSON.stringify({ command }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(handleSSEResponse);
+
+    sse.then(async (events) => {
+      for await (const event of events) {
+        if (event.data) {
+          stdout.innerText += event.data;
+          stdout.innerText += "\n";
+        }
+      }
+    });
+  })
+);
+
+submitCommand$.subscribe();
