1a11d505015992cc2eb11392a958f8b623cab630
Chauqni Sun <chuanqi.sun.dev@gmail.com>
2024-12-15 15:52:38 -0800

    poc stream tool use

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -5,7 +5,7 @@ import { debounceTime, fromEvent, map, tap } from "rxjs";
 import { handleOpenMenu } from "./handlers/handle-open-menu";
 import { $, $all, $new, getDetail, parseActionEvent } from "./lib/dom";
 import { Environment, type ObjectsChangeEventDetail, type ThreadChangeEventDetail } from "./lib/environment";
-import { system, user } from "./lib/messages";
+import { system } from "./lib/messages";
 import { OpenAILLMProvider } from "./lib/openai-llm-provider";
 import { defineSettingsElement } from "./lib/settings-element";
 
@@ -51,22 +51,48 @@ input.addEventListener("keydown", async (e) => {
 
     input.value = "";
     const id = ++taskId;
-    stdout.append($new("div", { "data-role": "user" }, [prompt]));
+    stdout.append($new("div", { "data-role": "user" }, [`<speak>${prompt}</speak>`]));
     const execId = env.exec(prompt);
 
+    function readFile(props: { filename: string }) {
+      return `Hello world!`;
+    }
+
     const aoai = await openai.getClient("aoai");
-    const response = await aoai.chat.completions.create({
+    const response = await aoai.beta.chat.completions.runTools({
       stream: true,
+      tools: [
+        {
+          type: "function",
+          function: {
+            function: readFile,
+            description: "Read a file uploaded by the user",
+            parse: JSON.parse,
+            parameters: {
+              type: "object",
+              properties: {
+                filename: { type: "string" },
+              },
+            },
+          },
+        },
+      ],
       messages: [
-        system`Respond in colloquial utterance.`,
+        system`
+Respond in a custom xml language. You can only use the following tags:
+
+<speak>Your verbal response. Use this for short simple response.</speak>
+<show mime-type="..." name="...>Standalone text response. You can show formatted source code or rich text</show>
+<think>Your private thoughts. Think before you speak, show, or script when the task is complex.</think>
+
+When you use <show>, only these mime-types are supported: text/plain, text/markdown, text/html, application/javascript, application/json. Pick the most suitable type for the task.
+When you use <script>, you have access to modern JavaScript APIs.
+        `,
         // TODO collect history from thread, not stdout
-        ...[...$all("[data-role]")].map(
-          (div) => ({
-            role: div.getAttribute("data-role") as "assistant" | "user",
-            content: div.textContent ?? "",
-          }),
-          user`${prompt}`
-        ),
+        ...[...$all("[data-role]")].map((div) => ({
+          role: div.getAttribute("data-role") as "assistant" | "user",
+          content: div.textContent ?? "",
+        })),
       ],
       model: "gpt-4o",
     });
