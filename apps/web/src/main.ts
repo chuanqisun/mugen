8f531ee08fd07f76bc608f86e47ccb186919df30
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2025-01-03 21:23:16 -0800

    share textarea for both input and output

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -23,6 +23,79 @@ const windowClick$ = fromEvent(window, "click").pipe(
   })
 );
 
+const runMessage$ = fromEvent(blocks, "run-message").pipe(
+  tap(async (e) => {
+    const block = (e.target as HTMLElement).closest("code-editor-element") as CodeEditorElement;
+    const prompt = block.value;
+
+    const openai = await llm.getClient();
+    const task = await openai.chat.completions.create({
+      stream: true,
+      model: "gpt-4o-mini",
+      messages: [
+        system`
+Respond with text blocks like this: <block filename="name.ext">text content</block>. Default to response.md`,
+        user`<block>test input</block>`,
+        assistant`<block filename="test.txt">test output</block>`,
+        user`<block>${prompt}</block>`,
+      ],
+    });
+
+    let currentObjectPath: string | null = null;
+    let currentEditor: CodeEditorElement | null = null;
+    let shouldTrimStart = true; // trim whitespace immediately before tag inner html starts. This allows artifact to have a clean looking start
+
+    const parser = new Parser({
+      onopentag(name, attributes, isImplied) {
+        if (isImplied) return;
+
+        if (name === "block") {
+          currentObjectPath = attributes.filename ?? "unnamed.txt";
+          shouldTrimStart = true;
+          const lang = currentObjectPath.split(".").pop() ?? "txt";
+          currentEditor = $new<CodeEditorElement>("code-editor-element", { "data-lang": lang });
+          const fileName = $new("div", {}, [currentObjectPath]);
+          block.insertAdjacentElement("afterend", fileName);
+          fileName.insertAdjacentElement("afterend", currentEditor);
+        } else if (currentEditor) {
+          const attributesString = Object.entries(attributes)
+            .map(([key, value]) => `${key}="${value}"`)
+            .join(" ");
+          const tagString = `<${name}${attributesString.length ? ` ${attributesString}` : ""}>`;
+          currentEditor?.appendText(tagString);
+        }
+      },
+
+      ontext(text) {
+        if (currentEditor) {
+          if (shouldTrimStart) {
+            text = text.trimStart();
+            shouldTrimStart = !text; // if text is empty, keep trimming
+          }
+          currentEditor.appendText(text);
+        }
+      },
+      onclosetag(name, isImplied) {
+        if (isImplied) return;
+
+        if (name === "block") {
+          currentEditor = null;
+          shouldTrimStart = true;
+        } else {
+          const tagString = `</${name}>`;
+          currentEditor?.appendText(tagString);
+        }
+      },
+    });
+
+    for await (const chunk of task) {
+      parser.write(chunk.choices[0]?.delta?.content || "");
+    }
+
+    parser.end();
+  })
+);
+
 const formSubmission$ = fromEvent(inputForm, "submit").pipe(
   tap(preventDefault),
   tap(async (e) => {
@@ -99,3 +172,4 @@ Respond with text blocks like this: <block filename="name.ext">text content</blo
 
 windowClick$.subscribe();
 formSubmission$.subscribe();
+runMessage$.subscribe();
