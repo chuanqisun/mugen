ef9d4b29ee1b3e3141c2b728dc9faf1b3cbd4c26
chuanqisun <chuanqi.sun.dev@gmail.com>
2025-01-16 08:37:00 -0800

    set up auto append

--- apps/web/src/settings/handle-open-menu.ts
+++ apps/web/src/settings/handle-open-menu.ts
@@ -3,6 +3,6 @@ import { $, type ParsedActionEvent } from "../utils/dom";
 export function handleOpenMenu(e: ParsedActionEvent) {
   if (e.action !== "open-menu") return;
 
-  const dialog = $("dialog")!;
+  const dialog = $<HTMLDialogElement>("dialog:has(settings-element)")!;
   dialog.showModal();
 }
