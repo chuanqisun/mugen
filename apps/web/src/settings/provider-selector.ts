950ad4889c24b9166d9e7fa97ce19963e74c9a89
Chauqni Sun <chuanqi.sun.dev@gmail.com>
2025-01-18 13:11:01 -0800

    add url based model selection

--- apps/web/src/settings/provider-selector.ts
+++ apps/web/src/settings/provider-selector.ts
@@ -1,38 +1,41 @@
-import { BehaviorSubject, combineLatest, fromEvent, map, mergeWith, startWith, Subject, tap } from "rxjs";
+import { html, render } from "lit";
+import { BehaviorSubject, combineLatest, fromEvent, map, merge, startWith, tap } from "rxjs";
 import type { BaseConnection, BaseProvider } from "../model-providers/base";
 import { createProvider } from "../model-providers/factory";
 import { $, getDetail } from "../utils/dom";
+import { useRouteParameter } from "../utils/route-parameter";
 import { connectionStore } from "./connections-store";
 
 export const activeProvider = new BehaviorSubject<BaseProvider | null>(null);
 
 export function useProviderSelector() {
-  const optionsChanged$ = new Subject<void>();
+  const routeConnectionId = useRouteParameter({ key: "connectionId" });
+
   const selectElement = $<HTMLSelectElement>("#active-connection")!;
 
-  const selectedId = fromEvent(selectElement, "change").pipe(
-    mergeWith(optionsChanged$),
-    map((_) => selectElement.value),
-    startWith(selectElement.value)
+  const reflectSelection$ = fromEvent(selectElement, "change").pipe(
+    map((e) => (e.target as HTMLSelectElement).value),
+    tap((id) => routeConnectionId.replace(id))
   );
 
-  const options$ = fromEvent(connectionStore, "change").pipe(
-    map(getDetail<BaseConnection[]>),
-    startWith(connectionStore.listConnections()),
-    tap((connections) => {
-      selectElement.innerHTML = `
-<option value="" disabled>Select a connection</option>
-${connections.map((connection) => `<option value="${connection.id}">${connection.displayName}</option>`).join("")}
-      `;
+  const options$ = fromEvent(connectionStore, "change").pipe(map(getDetail<BaseConnection[]>), startWith(connectionStore.listConnections()));
+
+  const updateActiveProvider$ = combineLatest([routeConnectionId.value$, options$]).pipe(
+    tap(([latestId, connections]) => {
+      const isSelectionInvalid = connections.length && !connections.find((c) => c.id === latestId);
+      render(
+        html`
+          ${!connections.length ? html`<option value="" disabled selected>No connection available</option>` : null}
+          ${isSelectionInvalid ? html`<option value="" disabled selected>Select a connection</option>` : null}
+          ${connections.map((connection) => html`<option value="${connection.id}" ?selected=${connection.id === latestId}>${connection.displayName}</option>`)}
+        `,
+        selectElement
+      );
     }),
-    tap(() => optionsChanged$.next())
-  );
-
-  const updateActiveModel = combineLatest([selectedId, options$]).pipe(
-    map(([id, connections]) => connections.find((c) => c.id === id) ?? null),
-    map((connection) => (connection ? createProvider(connection.type) : null)),
+    map(([latestId, connections]) => connections.find((c) => c.id === latestId) ?? null),
+    map((connection) => (connection ? (createProvider(connection.type) as BaseProvider) : null)),
     tap((provider) => activeProvider.next(provider))
   );
 
-  return updateActiveModel;
+  return merge(reflectSelection$, updateActiveProvider$);
 }
