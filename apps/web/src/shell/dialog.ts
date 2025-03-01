06b914b9617b26209ea36da44d94d7c11320b568
chuanqisun <chuanqi.sun.dev@gmail.com>
2025-02-28 20:01:10 -0800

    init minimum ui

--- apps/web/src/shell/dialog.ts
+++
@@ -1,8 +0,0 @@
-import { $ } from "../utils/dom";
-
-export function showDialog(html: string) {
-  const dialogElement = $<HTMLDialogElement>("#app-dialog")!;
-  dialogElement!.innerHTML = html;
-  dialogElement.addEventListener("close", () => (dialogElement.innerHTML = ""), { once: true });
-  dialogElement.showModal();
-}
